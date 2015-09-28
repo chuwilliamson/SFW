0a169f4c85faf303488be039d96504af89d3fc84
chuwilliamson <chuwilliamson@gmail.com>
2015-09-28 10:56:59 -0500

    hiding things

--- example project/sfwexample.cpp
+++ example project/sfwexample.cpp
@@ -18,48 +18,52 @@
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-
+using namespace sfw;
 void main()
 {
 	//first we need to initialize our graphics context- or window.
-	sfw::initContext(SCREEN_WIDTH, SCREEN_HEIGHT, "Dinosaurs!");
+	initContext(SCREEN_WIDTH, SCREEN_HEIGHT, "Dinosaurs!");
 
 	
 	float xpos = 300, ypos = 400; // position
 	float xacc = 0, yacc = 0;	  // acceleration
 	float xvel = 0, yvel = 0;	  // velocity
 	float speed = 100;
-
+	bool pooped = false;
 	// then we need to update our buffers, poll for input, etc.
-	while (sfw::stepContext())
+	while (stepContext())
 	{
 		// we haven't covered frame independent movement yet,
 			// but we'll use some euler integration
 		// we also haven't gotten into vector math yet,
 			// so we'll use cardinal speeds 
-		xpos += xvel * sfw::getDeltaTime();
-		ypos += yvel * sfw::getDeltaTime();
+		xpos += xvel * getDeltaTime();
+		ypos += yvel * getDeltaTime();
 
 		// base acceleration on the keypress
 		yacc = xacc = 0;
-		if (sfw::getKey('a')) xacc = -speed;
-		if (sfw::getKey('d')) xacc = speed;
-		if (sfw::getKey('w')) yacc = -speed;
-		if (sfw::getKey('s')) yacc = speed;
+		if (getKey('a')) xacc = -speed;
+		if (getKey('d')) xacc = speed;
+		if (getKey('w')) yacc = -speed;
+		if (getKey('s')) yacc = speed;
 		
 		// more euler integration
-		xvel += xacc * sfw::getDeltaTime();
-		yvel += yacc * sfw::getDeltaTime();
+		xvel += xacc * getDeltaTime();
+		yvel += yacc * getDeltaTime();
 
-		sfw::drawCircle(xpos, ypos, 30);
-
-		sfw::drawLine(5, 5, 5, SCREEN_HEIGHT-5);
-		sfw::drawLine(5, 5, SCREEN_WIDTH-5, 5);
-		
-		sfw::drawLine(SCREEN_WIDTH-5, SCREEN_HEIGHT-5, 5, SCREEN_HEIGHT-5);
-		sfw::drawLine(SCREEN_WIDTH - 5, SCREEN_HEIGHT - 5, SCREEN_WIDTH- 5, 5);
+		drawCircle(xpos, ypos, 30);
+		if (getKey('f'))
+		{
+			pooped = !pooped;
+			
+		}
+		if(pooped)
+			drawCircle(xpos, ypos, 5);
+			
+		drawLine(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		 
 	}
 
 	//finally, we need to properly shut stuff down.
-	sfw::termContext();
+	termContext();
 }
